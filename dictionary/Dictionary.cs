c9a0818e88d87da302155787790c494834f397e1
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-09-18 18:02:05 +0200

    Dictionary

--- dictionary/Dictionary.cs
+++ dictionary/Dictionary.cs
@@ -5,11 +5,11 @@ namespace Dictionary
 {
     class Program
     {
-        static Dictionary<string, string> Dictionary;
+        static Dictionary<string, string> dictionary;
 
         static void Main(string[] args)
         {
-            Dictionary = new Dictionary<string, string>
+            Dictionary<string, string> dic = new Dictionary<string, string>
             {
                 { "alma", "apple" },
                 { "fa", "tree" }
@@ -17,25 +17,39 @@ namespace Dictionary
 
             //add more words to your dictionary
 
+            dic.Add("kutya", "dog");
+            dic.Add("macska", "cat");
+
+            AddWord(dic, "kacsa", "duck");
+            RemoveWord("alma", dic);
+            Console.WriteLine(TranslateToEnglish("kacsa", dic));
+
         }
 
         // Implement this method. It should add the given key-value pair to the the dictionary
-        public static void AddWord(string hungarianWord, string englishWord)
+        public static void AddWord(Dictionary<string, string> dict, string hungarianWord, string englishWord)
         {
-
+            if (dict.ContainsKey(hungarianWord))
+            {
+                dict[hungarianWord] = englishWord;
+            }
+            else
+            {
+                dict.Add(hungarianWord, englishWord);
+            }
         }
 
         // Implement this method. It should remove the key-value pair by the given key from the dictionary
-        public static void RemoveWord(string hungarianWord)
+        public static void RemoveWord(string hungarianWord, Dictionary<string, string> dictionary)
         {
-
+            dictionary.Remove(hungarianWord);
         }
 
         // Implement a method which works as a translator from Hungarian to English
         // Example: you give it a parameter "fa" and it's output is "tree"
-        public static string TranslateToEnglish(string hungarian)
+        public static string TranslateToEnglish(string hungarianWord, Dictionary<string, string> dictionary)
         {
-
+            return (dictionary[hungarianWord]);
         }
 
         // Implement a method which works as a translator from English to Hungarian
